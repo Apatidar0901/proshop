e2cb97a1ca8e7ad83a5a535992d58efd04f3a704
Abhishek Patidar <apatidar0901@gmail.com>
2025-07-21 08:44:07 +0530

    Database - MongoDB and Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
-import products from "./data/products.js";
+dotenv.config();
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import productRoutes from "./routes/productRoutes.js";
 const port = process.env.PORT || 5000;
 
+connectDB(); //connect to MongoDB
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product || { message: "Product not found" });
-});
+app.use(notFound); // Middleware for handling 404 errors
+app.use(errorHandler); // Middleware for handling errors
 
 app.listen(port, () => console.log("server running"));
